abd301c680901f301fce2744abe6727b4f4f77ff
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-12-23 04:06:12 +0600

    feat: home screen content

--- lib/shared/constants/app_colors.dart
+++ lib/shared/constants/app_colors.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color.fromARGB(100, 33, 29, 226);
+
+  static const Color textPrimary = Color.fromARGB(100, 240, 240, 243);
+  static const Color textSecondary = Color.fromARGB(98, 193, 193, 196);
+
+  static const Color shadow50 = Color.fromRGBO(49, 69, 106, 0.25);
+  static const Color shadow15 = Color.fromRGBO(49, 69, 106, 0.15);
+  static const Color shadow05 = Color.fromRGBO(49, 69, 106, 0.05);
+  static const Color shadowInput15 = Color.fromRGBO(55, 84, 170, 0.15);
 }
--- lib/widgets/screens/home.dart
+++ lib/widgets/screens/home.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:pet_id_checker/shared/constants/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      appBar: AppBar(
+        title: const Text(
+          'PetID PreSell Checker',
+          style: TextStyle(
+              color: AppColors.textPrimary, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: AppColors.primary,
+      ),
+      body: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Center(
+                          child:
+                          Image(image: AssetImage('src/assets/admin-card.png')),
+                      )
+                  ),
+                  Text(
+                      'This application is created to check Tags before sell them. All Tag boxes must contain control QR codes that will show is Tag already on use or not.',
+                      style: TextStyle(color: AppColors.textSecondary)
+                  )
+                ]
+            ),
 
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: null,
+                    child: Text('Test',
+                        style: TextStyle(color: AppColors.textPrimary)
+                    ),
+                )
+              ],
+            )
+          ]
+      ),
+    );
+  }
 }
