f788087d4fe37566eef250174b5ee08f9d018b0d
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-12-23 15:52:45 +0600

    fix: ui rework

--- lib/shared/constants/app_colors.dart
+++ lib/shared/constants/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color.fromARGB(200, 33, 29, 226);
+  static const Color primary = Color.fromARGB(220, 33, 29, 226);
 
   static const Color textPrimary = Color.fromRGBO(250, 250, 250, 1.0);
   static const Color textSecondary = Color.fromRGBO(220, 220, 220, 1.0);
--- lib/widgets/screens/home.dart
+++ lib/widgets/screens/home.dart
@@ -8,50 +8,51 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
-      appBar: AppBar(
-        title: const Text(
-          'PetID PreSell Checker',
-          style: TextStyle(
-              color: AppColors.textPrimary, fontWeight: FontWeight.bold
-          ),
-        ),
-        backgroundColor: AppColors.primary,
-      ),
-      body: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: Container(
+        color: AppColors.primary,
+        child: const Column(
           children: [
-            Padding(
-              padding: EdgeInsets.all(20.0), 
+            Expanded(
+              flex: 3,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Center(
-                      child:
-                      Image(image: AssetImage('lib/assets/admin_card.png')),
+                  SizedBox(height: 128.0),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    child: Center(
+                      child: Image(image: AssetImage('lib/assets/admin_card.png'))
+                    ),
                   ),
                   Center(
-                    child: Text(
-                      'This application is created to check Tags before sell them. All Tag boxes must contain control QR codes that will show is Tag already on use or not.',
-                      style: TextStyle(color: Colors.white)
+                    child: Text('PetID PreSell Checker', style: TextStyle(fontSize: 28, color: AppColors.textPrimary, fontWeight: FontWeight.bold))
+                  ),
+                  SizedBox(height: 16.0),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 60.0),
+                    child: Center(
+                      child: Text(
+                        'This application is created to check Tags before selling them. All Tag boxes must contain control QR codes that will show whether Tag is already in use or not.', 
+                        style: TextStyle(
+                          fontSize: 12, color: AppColors.textSecondary
+                        )
+                      )
                     ),
-                  )
-                ]
-              ),
+                  ),
+                ],
+              )
             ),
-
-            Column(
-              children: [
-                ElevatedButton(
-                  onPressed: null,
-                    child: Text('Test',
-                        style: TextStyle(color: AppColors.textPrimary)
-                    ),
-                )
-              ],
+            Expanded(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: null,
+                    child: Text('Test', style: TextStyle(color: AppColors.textPrimary)),
+                  )
+                ],
+              ),
             )
-          ]
+          ],
+        ),
       ),
     );
   }
