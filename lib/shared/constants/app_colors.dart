620e81eaa33e8e551535cf83340c3d8020909756
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-12-24 06:08:14 +0600

    feat: finish qr scanner

--- lib/shared/constants/app_colors.dart
+++ lib/shared/constants/app_colors.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color.fromARGB(220, 33, 29, 226);
+  static const Color primaryBright = Color.fromARGB(255, 33, 29, 226);
 
   static const Color textPrimary = Color.fromRGBO(250, 250, 250, 1.0);
   static const Color textSecondary = Color.fromRGBO(220, 220, 220, 1.0);
+
+  static const Color shadowPrimary = Color.fromARGB(180, 0, 0, 0);
 
   static const Color shadow50 = Color.fromRGBO(49, 69, 106, 0.25);
   static const Color shadow15 = Color.fromRGBO(49, 69, 106, 0.15);
--- lib/widgets/screens/scanner.dart
+++ lib/widgets/screens/scanner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:pet_id_checker/shared/constants/app_colors.dart';
+import 'package:pet_id_checker/widgets/components/qr_scanner_overlay.dart';
 
 class ScannerScreen extends StatelessWidget {
   const ScannerScreen({super.key});
@@ -14,6 +15,7 @@ class ScannerScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.primary,
       appBar: AppBar(
+        foregroundColor: AppColors.textSecondary,
         backgroundColor: AppColors.primary,
         title: const Text('PreSell Checker Scanner', style: TextStyle(color: AppColors.textPrimary, fontWeight: FontWeight.bold)),
         // actions: [], - add close scanner
@@ -22,18 +24,22 @@ class ScannerScreen extends StatelessWidget {
         width: double.infinity,
         child: Column(
             children: [
-              const Expanded(
-                child: Column(
-                  children: [
-                    SizedBox(height: 64.0),
-                    Text('Place the QR code of the box in the area', style: TextStyle(fontSize: 16, color: AppColors.textPrimary, fontWeight: FontWeight.bold))
-                  ],
-                ),
-              ),
               Expanded(
-                flex: 6,
-                child: MobileScanner(onDetect: _onScan)
-              )
+                child: Stack(
+                  children: [
+                    MobileScanner(
+                      onDetect: _onScan,
+                      overlay: const Column(
+                        children: [
+                          // SizedBox(height: 64.0),
+                          // Text('Place the QR code of the box in the area', style: TextStyle(fontSize: 16, color: AppColors.textPrimary, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                    const QRScannerOverlay(overlayColour: AppColors.shadowPrimary)
+                  ]
+                )
+              ),
             ],
           ),
       ),
