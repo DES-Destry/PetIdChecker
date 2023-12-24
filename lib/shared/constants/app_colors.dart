21aa31ba5d01e110908e8b613acd1fa8fbefc7a4
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-12-25 03:18:22 +0600

    fix: make qr scanner border brighter

--- lib/shared/constants/app_colors.dart
+++ lib/shared/constants/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color.fromARGB(220, 33, 29, 226);
-  static const Color primaryBright = Color.fromARGB(255, 33, 29, 226);
+  static const Color primaryBright = Color.fromARGB(255, 128, 128, 255);
 
   static const Color textPrimary = Color.fromRGBO(250, 250, 250, 1.0);
   static const Color textSecondary = Color.fromRGBO(220, 220, 220, 1.0);
